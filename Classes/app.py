465ae0bf70b175fde651f6532ed50df8acfb5e15
Matheus Maranhão <matheusmaranhao02@gmail.com>
2023-12-17 16:05:57 -0300

    Adicionada a função de aumento para Desenvolvedor

--- Classes/app.py
+++ Classes/app.py
@@ -11,7 +11,7 @@ class Empregados:
         self.trabalhando = trabalhando
         self.burnout = False  # Adicione essa linha para evitar erros
 
-        print(f'O empregado {self.nome_completo}, foi contratado, com um salário de {self.salario}!'
+        print(f'{self.nome_completo}, foi contratado, com um salário de {self.salario}!'
               + f'\n\t Matricula: {self.matricula_funcional}'
               + f'\n\t Email Profissional: {self.email}')
 
@@ -35,11 +35,12 @@ class Empregados:
 class Desenvolvedor(Empregados):
     porcentagem_aumento = 0.08
 
-    def __init__(self, nome_completo, email, matricula_funcional, salario, trabalhando, linguagens_programacao, litros_cafe, burnout):
+    def __init__(self, nome_completo, email, matricula_funcional, salario, trabalhando, linguagens_programacao, litros_cafe, burnout, projetos_finalizados):
         super(Desenvolvedor, self).__init__(nome_completo, email, matricula_funcional, salario, trabalhando)
         self.linguagens_programacao = linguagens_programacao
         self.litros_cafe = litros_cafe
         self.burnout = burnout
+        self.projetos_finalizados = 0
         print(f'O Empregado {self.nome_completo}, foi contratado como desenvolvedor!)')
 
     def adicionar_linguagem(self, linguagem):
@@ -51,6 +52,16 @@ class Desenvolvedor(Empregados):
             self.burnout = True
         else:
             self.burnout = False
+            print(f'O dev {self.nome_completo} bebeu {litros_cafe} litros de café')
+
+    def receber_aumento(self, projetos_finalizados):
+                if projetos_finalizados >= 3 and len(self.linguagens_programacao) >=2:
+                    salario_aumentado = (self.salario * self.porcentagem_aumento) + self.salario
+                    print(f'O dev {self.nome_completo}, recebeu um aumento de R$ {self.salario} para R$ {salario_aumentado}.')
+                    self.salario = salario_aumentado
+                    
+                else:
+                    print(f'O dev {self.nome_completo}, ainda não atende aos requisitos para receber um aumento')
 
 
 class GerenteProjeto(Empregados):
@@ -62,8 +73,8 @@ class GerenteProjeto(Empregados):
         self.projetos_envolvidos = projetos_envolvidos
         print(f'O Empregado {self.nome_completo}, foi contratado como um Gerente!)')
 
-    def adicionar_desenvolvedor(self, desenvolvedor):
-        self.time.append(desenvolvedor)
+    def adicionar_desenvolvedor(self, time):
+        self.time.append(Desenvolvedor)
 
     def remover_desenvolvedor(self):
         pass
@@ -75,23 +86,24 @@ class GerenteProjeto(Empregados):
 # ---------------------------------------------------------------------------------
 
 desenvolvedor1 = Desenvolvedor(
-    nome_completo="Matheus",
+    nome_completo="Matheus Santos",
     email="matheus@example.com",
     matricula_funcional="001",
-    salario=50000,  # Substitua pelo salário desejado
+    salario=10000,  # Substitua pelo salário desejado
     trabalhando=False,  # Comece com False
     linguagens_programacao=["Python", "JavaScript"],
     litros_cafe=0,  # Comece com 0
     burnout=False,  # Comece com False
+    projetos_finalizados=0, #Comece com 0
 )
 
 sleep(3.00)
-
 desenvolvedor1.iniciar_jornada()
-
-sleep(3.00)
+sleep(2.00)
 desenvolvedor1.beber_cafe(1.0)  # Passe quantos litros de café o dev bebeu
-desenvolvedor1.adicionar_linguagem("C++")
-
-sleep(3.00)
+sleep(2.00)
+desenvolvedor1.adicionar_linguagem("C++") # Adicione uma linguagem que o dev aprendeu
+sleep(2.00)
+desenvolvedor1.receber_aumento(3) # Passe quantos projetos o dev finalizou
+sleep(2.00)
 desenvolvedor1.finalizar_jornada()
